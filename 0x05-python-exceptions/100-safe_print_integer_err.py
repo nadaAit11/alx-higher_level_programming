12ead27b16dfcf698eb1b10de60cf36316df72ac
Nada Ait <nadaait171@gmail.com>
2023-08-24 15:46:18 -0700

    exceptions

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return (False)
+
+    return (True)
