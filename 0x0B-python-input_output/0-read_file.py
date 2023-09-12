4ff87177cb20fed8392be99761afaec7c9c2026b
nada <nadaait171@gmail.com>
2023-09-12 20:32:18 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Read file"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """""reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
