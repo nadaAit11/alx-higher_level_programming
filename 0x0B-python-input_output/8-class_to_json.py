d96847a1f6ed51522d927cf439739b1b594e0840
nada <nadaait171@gmail.com>
2023-09-12 21:10:24 +0000

    fixes

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+"""Class to JSON"""
 
 
 def class_to_json(obj):
-    """ function that returns the dictionary
-    description
-    """
-    dic = {}
-    if hasattr(obj, "__dict__"):
-        dic = obj.__dict__.copy()
-    return dic
+    """returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return obj.__dict__
