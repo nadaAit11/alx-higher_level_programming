31b43c73cdc40b0721a375d2264c5ca9019bb856
nada <nadaait171@gmail.com>
2023-11-14 15:19:53 -0800

    rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is not a positive integer, create an empty object
+      Object.create(null);
+    }
+  }
+
+  print () {
+    // Print the rectangle using the character 'X'
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let tmp = width;
+    width = height;
+    height = tmp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
