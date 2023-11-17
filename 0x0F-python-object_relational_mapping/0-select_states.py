2990b71d6376f5bd722ac3bf6e21c7b555d875f3
nada <nadaait171@gmail.com>
2023-11-17 13:40:56 -0800

    fixing issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,41 @@
 #!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-def list_states(username, password, database):
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    # Create a cursor object to execute SQL queries
+def list_states(username, password, database):
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=username,
+        passwd=password, db=database
+    )
+
+    # Prepare a cursor object using cursor() method
     cursor = db.cursor()
 
-    # Execute the query to retrieve the list of states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Execute SQL query
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
 
-    # Fetch all the rows and display the results
-    states = cursor.fetchall()
-    for s in states:
-        print(s)
+    # Fetch all rows and display results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
 
-    # Close the cursor and the database connection
-    cursor.close()
+    # Disconnect from server
     db.close()
 
+
 if __name__ == "__main__":
+    # Retrieve arguments from command line
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
     list_states(username, password, database)
