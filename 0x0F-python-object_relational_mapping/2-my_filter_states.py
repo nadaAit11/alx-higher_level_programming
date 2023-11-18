30c2dcabe7451e766254d3498461544764f4acb2
nada <nadaait171@gmail.com>
2023-11-17 17:28:53 -0800

    filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ where name matches matches an argument passed as a parameter
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
