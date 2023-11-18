c9e95bebd07e9486772d425b60c235b308efbd08
nada <nadaait171@gmail.com>
2023-11-17 17:18:03 -0800

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     # through the same connection to the database.
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name\
-                LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name
+                LIKE BINARY 'N%'\ ORDER BY id ASC")
 
     rows = cur.fetchall()
     for i in rows:
