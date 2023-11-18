5c46b619ffa98205cd1e852c2a932bcde00bfdbd
nada <nadaait171@gmail.com>
2023-11-17 17:00:39 -0800

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,41 +1,16 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with N (upper N)
+   from the database 'hbtn_0e_0_usa'"""
 
 import MySQLdb
 import sys
 
-
-def list_states(username, password, database):
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=username,
-        passwd=password, db=database
-    )
-
-    # Prepare a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # Execute SQL query
-    query = "SELECT * FROM states where name like 'N%' ORDER BY id ASC"
-    cursor.execute(query)
-
-    # Fetch all rows and display results
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    # Disconnect from server
-    db.close()
-
-
 if __name__ == "__main__":
-    # Retrieve arguments from command line
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states where name like 'N%'")
+    for data in cursor.fetchall():
+            print(data)
+    cursor.close()
+    db.close()
