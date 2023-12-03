3f8e932f9cfb7194ef21b61ea2cb13494b823d8a
nada <nadaait171@gmail.com>
2023-12-03 11:01:28 -0800

    updates

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a PUT request to 0.0.0.0:5000/catch_me with a specific user_id and header
-curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98"
+curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
