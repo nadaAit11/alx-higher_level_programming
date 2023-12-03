7530f17624673fcf18263a07b3c66b6a100279af
nada <nadaait171@gmail.com>
2023-12-03 10:51:11 -0800

    advanced

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,8 @@
 """
 Finds the peak value in a list of integers using binary search algorithm.
 """
+
+
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers"""
     if not list_of_integers:
