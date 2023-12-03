72b8c029a05f42111448caa03aa90231e4ec2a38
nada <nadaait171@gmail.com>
2023-12-03 11:08:18 -0800

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """fetches https://intranet.hbtn.io/status"""
-import urllib.request as request
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
         html = r.read()
         print('Body response:')
         print("\t- type: {}".format(type(html)))
