7179f497c298fc8602ad6762dd288e84b6a70b31
nada <nadaait171@gmail.com>
2023-12-03 11:15:45 -0800

    tasks

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""lists the most recent 10 commits from a passed repository/owner"""
+"""
+This script retrieves the 10 most recent commits from a specified GitHub
+repository using the GitHub API. The commits are printed in the format:
+'<sha>: <author name>'
+"""
+import requests
 from sys import argv
-from requests import get
+
+
+def main():
+    repo_name = argv[1]
+    owner_name = argv[2]
+    url = (f'https://api.github.com/repos/{owner_name}/{repo_name}/commits')
+
+    response = requests.get(url)
+    commits = response.json()
+
+    sorted_commits = sorted(
+        commits,
+        key=lambda commit: commit['commit']['author']['date'],
+        reverse=True
+    )
+
+    for i, commit in enumerate(sorted_commits):
+        if i == 10:
+            break
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos"
-    url += "/{}/{}/commits".format(argv[2], argv[1])
-    request = get(url)
-    commits = request.json()
-    try:
-        for i in range(10):
-            print(commits.get('sha'), end=': ')
-            print(commits.get('commit').get('author').get('name'))
-    except IndexError:
-        pass
+    main()
