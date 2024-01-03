c5460f4449637acda8da171c60963b1fb173d4ec
nada <nadaait171@gmail.com>
2024-01-03 13:17:48 -0800

    advanced

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,26 +5,23 @@
 */
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
-  if (error) console.log(error);
-  else {
-    const characters = JSON.parse(body).characters;
-    characters_dict = {};
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (error) console.log(error);
-        else {
-          let id = JSON.parse(body).url;
-          id = id.replace('https://swapi-api.hbtn.io/api/people/', '');
-          id = id.replace('/', '');
-          const name = JSON.parse(body).name;
-          characters_dict[id] = (name);
-        }
-      });
-    });
-    for (let id in characters_dict) {
-        console.log(characters_dict[id]);
-    }
+  if (!error && response.statusCode === 200) {
+    const film = JSON.parse(body);
+    printCharacters(film.characters, 0);
   }
 });
+
+function printCharacters (characters, i) {
+  if (i >= characters.length) {
+    return;
+  }
+
+  request(characters[i], function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, i + 1);
+    }
+  });
 }
